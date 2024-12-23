1b09a6518cc056e92f94d2f0e8b4b0620115d866
batman8056 <manimanish8056@gmail.com>
2024-12-23 21:42:19 +0530

    understand

---
+++ Node/2 Chapter/index.js
@@ -0,0 +1,13 @@
+// when i am using local so node inbuild function i called like that
+const fs = require ("fs");
+
+// create a file and write a sentance
+fs.writeFile("message1.txt","finaly i understand",(err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  }); 
+// we need to give utf8 file format then only it is readable otherwise like buffer number can be sowen
+  fs.readFile('./message1.txt',"utf8",(err, data) => {
+    if (err) throw err;
+    console.log(data);
+  }); 
