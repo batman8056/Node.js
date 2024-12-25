67f771ab69d432cb06f117faddc8ce0a0cb6ebb2
batman8056 <manimanish8056@gmail.com>
2024-12-25 22:27:01 +0530

    buffer

--- 2 Chapter/buffer.js
+++ 2 Chapter/buffer.js
@@ -1,3 +1,15 @@
 
 //buffer is global object not requird to import
 
+const buf1 = Buffer.from('123456789');
+const buf2 = Buffer.from('Hello');
+
+buf2.copy(buf1,2);//copy from buf2 to buf1 (target, target start )
+
+console.log(buf1.toString());//change as a string bufq
+
+console.log(Buffer.concat([buf1,buf2]).toString());//concat buf1 buf2 
+
+console.log(buf1.equals(buf2));//true or false
+
+console.log(Buffer.compare(buf1, buf2)); //to know diffrent between string -1
