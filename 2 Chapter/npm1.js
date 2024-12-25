f98c4dff0010041d1e7e72aa26d8e8356a16cfda
batman8056 <manimanish8056@gmail.com>
2024-12-25 21:17:16 +0530

    update

--- 2 Chapter/npm1.js
+++ 2 Chapter/npm1.js
@@ -2,8 +2,9 @@
 
 import generateName from "sillyname";
 var sillyName = generateName();
-console.log(`My name is ${sillyName}`);
+console.log(`My name is ${sillyName},`);
 
 import {randomSuperhero} from 'superheroes';
 var Superheroname = randomSuperhero();
-console.log(`My Superhero name is ${Superheroname}`);
+console.log(`My Superhero name is ${Superheroname} !!!`);
+
