93ea43c49410b039a728fde8916e79cc285d2600
batman8056 <manimanish8056@gmail.com>
2024-12-30 13:58:43 +0530

    notes

--- 2.1 Chapter/index1.js
+++ 2.1 Chapter/index1.js
@@ -1,4 +1,6 @@
 import express from "express";
+import morgan from 'morgan';
+import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -6,8 +8,18 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+//log details showed
+app.use(morgan('combined'));
+//create a body for URL-encoding request like our form submission.
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+  
+});
+//from action we define here /submit it was present index.html
+app.post("/submit", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
--- 2.1 Chapter/index2.js
+++ 2.1 Chapter/index2.js
@@ -3,6 +3,14 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next) {
+  console.log("Request Method: ", req.method);
+  console.log("Request URL: ", req.url);
+  next();//if we use another next middleware method it will move
+}
+//using middleware use logger
+app.use(logger);
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
