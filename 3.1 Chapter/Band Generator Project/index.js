68d7f9ad853e97445a3c7503498fbbf5d00da101
batman8056 <manimanish8056@gmail.com>
2025-01-02 22:35:45 +0530

    bug fix

--- 3.1 Chapter/Band Generator Project/index.js
+++ 3.1 Chapter/Band Generator Project/index.js
@@ -21,14 +21,11 @@ app.use(express.static("public"));
 
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
-const currentYear = new Date().getFullYear();
-console.log(currentYear)
+
 
 //Step 1 - Make the get route work and render the index.ejs file.
 app.get("/", (req, res) => {
-  res.render("index",{
-    Year:currentYear
-  });
+  res.render("index");
 });
 
 app.post("/submit", (req, res) => {
