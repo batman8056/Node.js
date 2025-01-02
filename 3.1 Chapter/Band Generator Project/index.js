907da5acaef5d5a493973d4058f928696da0e449
batman8056 <manimanish8056@gmail.com>
2025-01-02 22:37:26 +0530

    bug fix

--- 3.1 Chapter/Band Generator Project/index.js
+++ 3.1 Chapter/Band Generator Project/index.js
@@ -4,6 +4,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 const app = express();
 const port = 3000;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(bodyParser.urlencoded({ extended: true }));
