23cf497c8e4bf90f0c223152c9b151389a10386c
batman8056 <manimanish8056@gmail.com>
2025-01-04 21:12:18 +0530

    bug fix

--- 4 Chapter/Server-side API Req/index.js
+++ 4 Chapter/Server-side API Req/index.js
@@ -35,6 +35,18 @@ app.post("/", async (req, res) => {
   // Step 3: If you get a 404 error (resource not found) from the API request.
   // Pass an error to the index.ejs to tell the user:
   // "No activities that match your criteria."
+  try {
+    const type = req.body.type
+    const participants = req.body.participants
+    const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`);
+    const result = response.data;
+    res.render("index.ejs", { data: result[Math.floor(Math.random() * result.length)] });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
 app.listen(port, () => {
